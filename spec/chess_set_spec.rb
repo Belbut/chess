3f4db95620c837a8a1914ca7805d811cb042dcd8
Belbut <80793911+Belbut@users.noreply.github.com>
2024-11-19 16:00:20 +0000

    feat: add empty board

---
+++ spec/chess_set_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../lib/chess_kit/chess_kit'
+
+describe ChessKit do
+  context 'a chess kit is made of' do
+    subject(:chess_kit) { described_class.new }
+    context 'one board' do
+      let(:empty_board) { chess_kit.board }
+      context 'a empty board' do
+        it 'is expected to be a 8x8 matrix' do
+          height = empty_board.size
+          width = empty_board.first.size
+          expect(height).to eq 8
+          expect(width).to eq 8
+        end
+      end
+    end
+
+    describe 'pieces' do
+      xit 'is expected to empty board' do
+        expect(true).to eq(true)
+      end
+    end
+  end
+end
