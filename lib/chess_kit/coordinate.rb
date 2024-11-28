340df07e145f555637d4d463c840f6a71ef214f7
Belbut <80793911+Belbut@users.noreply.github.com>
2024-11-28 13:24:33 +0000

    refactor(coordinate): rename variables and methods

--- lib/chess_kit/coordinate.rb
+++ lib/chess_kit/coordinate.rb
@@ -10,28 +10,28 @@ class Coordinate
     "(#{x}, #{y})c"
   end
 
-  def self.from_array(array_notation)
-    unless array_notation.size == 2
+  def self.from_array(position)
+    unless position.size == 2
       raise ArgumentError,
-            "Invalid notation: '#{array_notation}'. Must have exactly two elements"
+            "Invalid notation: '#{position}'. Must have exactly two elements"
     end
-    unless array_notation.all?(Integer)
+    unless position.all?(Integer)
       raise ArgumentError,
-            "Invalid notation: '#{array_notation}'. Elements must be integers"
+            "Invalid notation: '#{position}'. Elements must be integers"
     end
 
-    x, y = array_notation
+    x, y = position
 
     new(x, y)
   end
 
-  def self.from_string(string_notation)
-    column_letter = string_notation.upcase.scan(/[A-Z]/)
-    row_number = string_notation.scan(/\d/).join.to_i
+  def self.from_notation(position)
+    column_letter = position.upcase.scan(/[A-Z]/)
+    row_number = position.scan(/\d/).join.to_i
 
     unless column_letter.size == 1 && row_number.positive?
       raise ArgumentError,
-            "Invalid notation: '#{string_notation}'. Must contain exactly one letter and a number."
+            "Invalid notation: '#{position}'. Must contain exactly one letter and a number."
     end
 
     x = ('A'..'Z').find_index(column_letter.first)
--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -2,88 +2,88 @@
 
 require_relative '../lib/chess_kit/coordinate'
 describe Coordinate do
-  let(:array_notation_A1) { [0, 0] }
-  let(:string_notation_A1) { 'A1' }
+  let(:array_position_A1) { [0, 0] }
+  let(:notation_position_A1) { 'A1' }
   let(:coordinate_A1) { Coordinate.new(0, 0) }
 
-  let(:array_notation_C3) { [2, 2] }
-  let(:string_notation_C3) { 'C3' }
+  let(:array_position_C3) { [2, 2] }
+  let(:notation_position_C3) { 'C3' }
   let(:coordinate_C3) { Coordinate.new(2, 2) }
 
-  let(:array_notation_Z23) { [25, 23] }
-  let(:string_notation_Z23) { 'Z23' }
+  let(:array_position_Z23) { [25, 23] }
+  let(:notation_position_Z23) { 'Z23' }
   let(:coordinate_Z23) { Coordinate.new(25, 23) }
 
   let(:all_coordinates) do
-    { array_notation_A1: coordinate_A1, array_notation_C3: coordinate_C3, array_notation_Z23: coordinate_Z23,
-      string_notation_A1: coordinate_A1, string_notation_C3: coordinate_C3, string_notation_Z23: coordinate_Z23 }
+    { array_position_A1: coordinate_A1, array_position_C3: coordinate_C3, array_position_Z23: coordinate_Z23,
+      notation_position_A1: coordinate_A1, notation_position_C3: coordinate_C3, notation_position_Z23: coordinate_Z23 }
   end
 
   describe '.from_array' do
     context 'when the argument is in a valid notation' do
-      let(:all_array_notations) do
-        { array_notation_A1: array_notation_A1, array_notation_C3: array_notation_C3,
-          array_notation_Z23: array_notation_Z23 }
+      let(:all_array_positions) do
+        { array_position_A1: array_position_A1, array_position_C3: array_position_C3,
+          array_position_Z23: array_position_Z23 }
       end
 
       it 'is expected to return a Coordinate' do
-        all_array_notations.each_value do |position|
+        all_array_positions.each_value do |position|
           expect(Coordinate.from_array(position)).to be_a(Coordinate)
         end
       end
 
       it 'is expected to have correct x and y values' do
-        all_array_notations.each_key do |array_notation_key|
-          position = all_array_notations[array_notation_key]
-          coordinate = all_coordinates[array_notation_key]
+        all_array_positions.each_key do |array_position_key|
+          position = all_array_positions[array_position_key]
+          coordinate = all_coordinates[array_position_key]
 
           expect(Coordinate.from_array(position)).eql?(coordinate)
         end
       end
     end
     context 'when the argument is in a invalid notation' do
-      let(:invalid_array_notation1) { [1, 2, 3] }
-      let(:invalid_array_notation2) { [1] }
-      let(:invalid_array_notation3) { ['A', 2] }
+      let(:invalid_array_position1) { [1, 2, 3] }
+      let(:invalid_array_position2) { [1] }
+      let(:invalid_array_position3) { ['A', 2] }
 
       it '' do
-        expect { Coordinate.from_array(invalid_array_notation1) }.to raise_error(ArgumentError)
-        expect { Coordinate.from_array(invalid_array_notation2) }.to raise_error(ArgumentError)
-        expect { Coordinate.from_array(invalid_array_notation3) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_array(invalid_array_position1) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_array(invalid_array_position2) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_array(invalid_array_position3) }.to raise_error(ArgumentError)
       end
     end
   end
 
-  describe '.from_string' do
+  describe '.from_notation' do
     context 'when the argument is in a valid notation' do
-      let(:all_string_notations) do
-        { string_notation_A1: string_notation_A1, string_notation_C3: string_notation_C3,
-          string_notation_Z23: string_notation_Z23 }
+      let(:all_notation_positions) do
+        { notation_position_A1: notation_position_A1, notation_position_C3: notation_position_C3,
+          notation_position_Z23: notation_position_Z23 }
       end
       it 'is expected to return a Coordinate' do
-        all_string_notations.each_value do |position|
-          expect(Coordinate.from_string(position)).to be_a(Coordinate)
+        all_notation_positions.each_value do |position|
+          expect(Coordinate.from_notation(position)).to be_a(Coordinate)
         end
       end
 
       it 'is expected to have correct x and y values' do
-        all_string_notations.each_key do |string_notation_key|
-          position = all_string_notations[string_notation_key]
-          coordinate = all_coordinates[string_notation_key]
+        all_notation_positions.each_key do |notation_position_key|
+          position = all_notation_positions[notation_position_key]
+          coordinate = all_coordinates[notation_position_key]
 
-          expect(Coordinate.from_string(position)).eql?(coordinate)
+          expect(Coordinate.from_notation(position)).eql?(coordinate)
         end
       end
     end
     context 'when the argument is in a invalid notation' do
-      let(:invalid_string_notation1) { 'AA2' }
-      let(:invalid_string_notation2) { 'a0' }
-      let(:invalid_string_notation3) { 'AB' }
+      let(:invalid_notation_position1) { 'AA2' }
+      let(:invalid_notation_position2) { 'a0' }
+      let(:invalid_notation_position3) { 'AB' }
 
       it '' do
-        expect { Coordinate.from_string(invalid_string_notation1) }.to raise_error(ArgumentError)
-        expect { Coordinate.from_string(invalid_string_notation2) }.to raise_error(ArgumentError)
-        expect { Coordinate.from_string(invalid_string_notation3) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_notation(invalid_notation_position1) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_notation(invalid_notation_position2) }.to raise_error(ArgumentError)
+        expect { Coordinate.from_notation(invalid_notation_position3) }.to raise_error(ArgumentError)
       end
     end
   end
